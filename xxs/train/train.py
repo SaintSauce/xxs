f1e38a434e7afc85b01ee2e489420642c3a18500
Saintsauce <t.donghao.zeng@gmail.com>
2025-04-23 18:48:30 -0400

    commenting out imports for ppo, grpo

--- xxs/train/train.py
+++ xxs/train/train.py
@@ -2,8 +2,8 @@ import torch
 from xxs.utils.config import ConfigLoader
 
 from xxs.sft.sft_trainer import SFTTrainer
-from xxs.rl.ppo_trainer import PPOTrainer
-from xxs.rl.grpo_trainer import GRPOTrainer
+# from xxs.rl.ppo_trainer import PPOTrainer
+# from xxs.rl.grpo_trainer import GRPOTrainer
 
 def run_sft(config: ConfigLoader, device: torch.device):
     """ run supervised finetuning """
@@ -13,18 +13,18 @@ def run_sft(config: ConfigLoader, device: torch.device):
     trainer.prepare_data()
     trainer.train()
 
-def run_ppo(config: ConfigLoader, device: torch.device):
-    """ run PPO training """
+# def run_ppo(config: ConfigLoader, device: torch.device):
+#     """ run PPO training """
 
-    print("Running PPO...")
-    trainer = PPOTrainer(config, device)
-    trainer.prepare_data()
-    trainer.train()
+#     print("Running PPO...")
+#     trainer = PPOTrainer(config, device)
+#     trainer.prepare_data()
+#     trainer.train()
 
-def run_grpo(config: ConfigLoader, device: torch.device):
-    """ run GRPO training """
+# def run_grpo(config: ConfigLoader, device: torch.device):
+#     """ run GRPO training """
 
-    print("Running GRPO...")
-    trainer = GRPOTrainer(config, device)
-    trainer.prepare_data()
-    trainer.train()
+#     print("Running GRPO...")
+#     trainer = GRPOTrainer(config, device)
+#     trainer.prepare_data()
+#     trainer.train()
